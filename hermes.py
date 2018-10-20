6109148f59c1e7b2386e8aedbe7164a54d445040
Dominic Tassio <dominic.tassio@gmail.com>
2018-10-20 16:49:58 -0500

    standardize the GET parameters across routes

--- hermes.py
+++ hermes.py
@@ -9,7 +9,7 @@ initialize(database)
 
 @app.route('/api/v1/twitter/public_key/get', methods=['GET'])
 def twitter_public_key_get():
-    twitter_user_id = request.args.get('user_id', '')
+    twitter_user_id = request.args.get('twitter_user_id', '')
     result = twitter_public_keys_select(database, twitter_user_id)
 
     reponse = {
@@ -34,7 +34,7 @@ def twitter_public_key_get():
 
 @app.route('/api/v1/twitter/public_key/update', methods=['GET'])
 def twitter_public_key_update():
-    twitter_user_id = request.args.get('user_uid', '')
+    twitter_user_id = request.args.get('twttier_user_id', '')
     public_key = request.args.get('public_key', '')
     result = twitter_public_keys_insert_or_replace(database, twitter_user_id, public_key)
 
