8ef7067c242663738a2f3ab09892dc02e264a4ca
Carter Nelson <cartern@ksu.edu>
2018-10-20 02:27:26 -0500

    Database

--- hermes.py
+++ hermes.py
@@ -1,23 +1,29 @@
 from flask import Flask, request, jsonify
-
+from users import *
 
 app = Flask(__name__)
+database = 'hermes.db'
 
 
 @app.route('/user/find', methods=['GET'])
 def find_user():
     username = request.args.get('user_uid', '')
-    service = request.args.get('user_service', '')
-    return jsonify([username, service])
+    # service = request.args.get('user_service', '')
+    public_key = find_user_db(database, username)
+    # return jsonify([username, service])
+    return jsonify([public_key])
 
 
 @app.route('/user/create', methods=['GET'])
 def create_user():
     username = request.args.get('user_uid', '')
-    service = request.args.get('user_service', '')
+    # service = request.args.get('user_service', '')
     public_key = request.args.get('user_public_key', '')
-    return jsonify([username, service, public_key])
+    create_user_db(username, public_key)
+    # return jsonify([username, service, public_key])
+    return jsonify([username, public_key])
 
 
 if __name__ == '__main__':
+    initialize(database)
     app.run(host='0.0.0.0')
---
+++ users.py
@@ -0,0 +1,38 @@
+import os.path
+import sqlite3
+
+
+def initialize(database):
+    if not os.path.isfile(database):
+        conn = sqlite3.connect(database)
+        conn.execute('''CREATE TABLE USERS
+                 (ID INT PRIMARY KEY NOT NULL,
+                 TWITTERID INT UNIQUE NOT NULL,
+                 PUBLICKEY TEXT NOT NULL);''')
+        conn.close()
+    pass
+
+
+# def find_user_db(database, username, service):
+# #     pass
+# #
+# #
+# # def create_user_db(database, username, service, public_key):
+# #     pass
+
+def find_user_db(database, username):
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+    cursor.execute('''SELECT PUBLICKEY FROM USERS WHERE TWITERID=?''', (username,))
+    public_key = cursor.fetchone()
+    conn.close()
+    if public_key:
+        return public_key
+    return 'No user'
+
+
+def create_user_db(database, username, public_key):
+    conn = sqlite3.connect(database)
+    conn.execute('''INSERT INTO USERS(TWITTERID, PUBLICKEY) VALUES(?,?)''', (username, public_key))
+    conn.close()
+    pass
