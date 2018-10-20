0629e8076cc1f3b29f3694f55e1c7d436c67c482
Carter Nelson <cartern@ksu.edu>
2018-10-20 02:42:42 -0500

    Database

--- users.py
+++ users.py
@@ -1,15 +1,15 @@
-import os.path
 import sqlite3
 
 
 def initialize(database):
-    if not os.path.isfile(database):
-        conn = sqlite3.connect(database)
-        conn.execute('''CREATE TABLE USERS
-                 (ID INT PRIMARY KEY NOT NULL,
-                 TWITTERID INT UNIQUE NOT NULL,
-                 PUBLICKEY TEXT NOT NULL);''')
-        conn.close()
+    conn = sqlite3.connect(database)
+    cursor = conn.cursor()
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS USERS
+             (ID INT PRIMARY KEY NOT NULL,
+             TWITTERID INT UNIQUE NOT NULL,
+             PUBLICKEY TEXT NOT NULL);''')
+    conn.close()
     pass
 
 
@@ -23,6 +23,7 @@ def initialize(database):
 def find_user_db(database, username):
     conn = sqlite3.connect(database)
     cursor = conn.cursor()
+
     cursor.execute('''SELECT PUBLICKEY FROM USERS WHERE TWITERID=?''', (username,))
     public_key = cursor.fetchone()
     conn.close()
@@ -33,6 +34,8 @@ def find_user_db(database, username):
 
 def create_user_db(database, username, public_key):
     conn = sqlite3.connect(database)
-    conn.execute('''INSERT INTO USERS(TWITTERID, PUBLICKEY) VALUES(?,?)''', (username, public_key))
+    cursor = conn.cursor()
+
+    cursor.execute('''INSERT INTO USERS(TWITTERID, PUBLICKEY) VALUES(?,?)''', (username, public_key))
     conn.close()
     pass
